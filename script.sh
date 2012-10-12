fe41c61cf9830fe257ac14d3d68442566aca5a28
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2012-10-13 00:36:53 +0200

    log in to minerva via curl

---
+++ script.sh
@@ -0,0 +1,36 @@
+
+username="$1"
+password="$2"
+cout="/tmp/cookie1"
+cin="/tmp/cookie2"
+temp1="/tmp/temp1"
+temp1="/tmp/temp1"
+
+swap_cookies() {
+    ctemp="$cout"
+    cout="$cin"
+    cin="$ctemp"
+}
+
+# Initializing cookies and retrieving authentication salt.
+curl --cookie-jar "$cout" "https://minerva.ugent.be/secure/index.php?external=true" --output "$temp1"
+swap_cookies
+
+
+salt=$(cat "$temp1" | sed '/authentication_salt/!d' | sed 's/.*value="\([^"]*\)".*/\1/')
+
+# Logging in.
+curl --cookie "$cin" --cookie-jar "$cout" \
+    --data "login=$username" \
+    --data "password=$password" \
+    --data "authentication_salt=$salt" \
+    --data "submitAuth=Log in" \
+    --location \
+    --output "$temp2" \
+        "https://minerva.ugent.be/secure/index.php?external=true"
+swap_cookies
+
+# Retrieving header page to parse.
+curl --cookie "$cin" --cookie-jar "$cout" "http://minerva.ugent.be/index.php" ---output "$temp1"
+
+# Parsing $temp1 and retrieving minerva document tree.
