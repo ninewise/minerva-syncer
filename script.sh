8d0c8b891e38cbd3bbc3ea3f557c07f2a0535bc9
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2012-12-01 11:58:09 +0100

    add user friendliness and (yet) unused functions

--- script.sh
+++ script.sh
@@ -1,37 +1,128 @@
+#!/usr/bin/env bash
 
-# CONFIG OPTIONS
-# TODO: enable choosing the following at first use and by command line parameters.
-DESTDIR=./Minerva
-DATAFILE="$DESTDIR/.mdata"
-
-
-cout="/tmp/cookie1"
-cin="/tmp/cookie2"
+# Some constants.
 temp1="/tmp/temp1"
 temp2="/tmp/temp2"
-temptree="/tmp/tree"
+temptree="/tmp/temptree"
+home="https://minerva.ugent.be/"
+configdir="$XDG_CONFIG_HOME/minerva-syncer"
 
-swap_cookies() {
-    ctemp="$cout"
-    cout="$cin"
-    cin="$ctemp"
+# Checks whether space separated $1 contains $2.
+contains() {
+    for member in $1; do
+        if [ "$member" == "$2" ]; then
+            return 0
+        fi
+    done
+    return 1
 }
 
-html_escape() {
-    # No more is needed, I think. Unless some scumbag teacher names his
-    # directories with weird characters...
+# Ask the user about $1. If $1 is a directory, ask whether to download
+# it as a whole, interactive, not at the moment or not ever. If $1 is a
+# normal file, ask whether to download it now, later, or never.
+ask_download_behaviour() {
+    if [ -d "$1" ]; then
+        question="Download the directory $1 [C]omplete, [i]nteractive, [l]ater or [n]ever?"
+        shorts="c i l n"
+    else
+        question="Download the file $1 [Y]es, [l]ater or [n]ever?"
+        shorts="y l n"
+    fi
+    read -p "$question " answer
+    while ! contains "$shorts" "${answer:0:1}"; do
+        read -p "Please reply with any of ($shorts). " answer
+    done
+    echo ${answer:0:1}
+}
+
+# The cookie files. We need two, as curl wants an in and an out cookie.
+# I alias the curl method to include a method which swaps the two
+# files.
+cin="/tmp/cookie1"
+cout="/tmp/cookie2"
+swap_cookies() {
+    ctemp="$cin"
+    cin="$cout"
+    cout="$ctemp"
+}
+
+# Methods to escape file names:
+# - Replace slashes and spaces from file names, so they can be used in
+#   the urls.
+# - Escape slashes to use filenames in sed substitutions.
+url_escape() {
     echo "$1" | sed -e 's_/_%2F_g' -e 's/ /%20/g'
 }
-
 sed_escape() {
     echo "$1" | sed -e 's_/_\\/_g'
 }
 
-# Reading username and password.
-read -p "Username: " username
-stty -echo
-read -p "Password: " password; echo
-stty echo
+# Let's start our main program.
+
+# First, loading the config file.
+if test -e "$configdir/config"; then
+    # Yes, this is not secure. Don't edit the file, then.
+    . "$configdir/config"
+else
+    # Ask the user some questions.
+    echo
+    echo "Welcome to minerva-syncer."
+    echo
+    echo "It seems this is the first time you're running the script."
+    echo "Before we start synchronizing with Minerva, I'll need to know"
+    echo "you're UGent username."
+    echo
+    read -p "UGent username: " username
+    echo
+    echo "Pleased to meet you, $username."
+    echo
+    echo "If you want, I could remember your password, so that you do"
+    echo "not have to enter it every time. However, since I have to"
+    echo "pass it to Minerva, I'll have to save it in plain text."
+    echo "Hit enter if you don't want your password saved."
+    echo
+    stty -echo
+    read -p "UGent password: " password; echo
+    stty echo
+    echo
+    echo "Now, which folder would you like so synchronize Minerva to?"
+    echo "If you're OK with \"~/Minerva\", just hit enter. Otherwise,"
+    echo "enter the absolute path to the folder you'd like to use."
+    echo
+    read -p "Destination directory: " destdir
+    echo
+    echo "OK, that's it. Let's get synchronizing."
+
+    # Default value.
+    if test -z "$destdir"; then
+        destdir=~/Minerva
+    fi
+
+    # Create the target directory if it does not yet exist.
+    if test ! -d "$destdir"; then
+        mkdir "$destdir"
+    fi
+    datafile="$destdir/.mdata"
+    date > "$datafile"
+
+    # Create the config directory.
+    if test ! -d "$configdir/"; then
+        mkdir "$configdir/"
+    fi
+
+    {
+        # Let's write a new config file.
+        echo "username=\"$username\""
+        echo "password=\"$password\""
+        echo "destdir=\"$destdir\""
+    } > "$configdir/config"
+fi
+
+if test -z "$password"; then
+    stty -echo
+    read -p "Password for $username: " password; echo
+    stty echo
+fi
 
 # Initializing cookies and retrieving authentication salt.
 echo -n "Initializing cookies and retrieving salt... "
@@ -67,7 +158,7 @@ cat "$temp1" | sed '/course_home.php?cidReq=/!d' | # filter lines with a course 
     cat - > "$temp2"
 
 # Make a hidden file system for the synchronizing.
-mkdir -p "$DESTDIR/.minerva"
+mkdir -p "$destdir/.minerva"
 
 for course in $(cat "$temp2"); do
     echo -n "."
@@ -76,7 +167,7 @@ for course in $(cat "$temp2"); do
     link="http://minerva.ugent.be/main/document/document.php?cidReq=$cidReq"
 
     # Make a directory for the course.
-    mkdir -p "$DESTDIR/.minerva/$name"
+    mkdir -p "$destdir/.minerva/$name"
 
     # Retrieving the course documents home.
     curl -b "$cin" -c "$cout" "$link" --output "$temp1" 2> /dev/null
@@ -93,11 +184,11 @@ for course in $(cat "$temp2"); do
     # For each directory.
     for folder in $folders; do
         # Make the folder in the hidden files system.
-        localdir="$DESTDIR/.minerva/$name/$folder"
+        localdir="$destdir/.minerva/$name/$folder"
         mkdir -p "$localdir"
 
         # Retrieving directory.
-        curl -b "$cin" -c "$cout" "$link&curdirpath=$(html_escape $folder)" --output "$temp1" 2> /dev/null
+        curl -b "$cin" -c "$cout" "$link&curdirpath=$(url_escape $folder)" --output "$temp1" 2> /dev/null
         swap_cookies
 
         # Parsing files from the directory.
@@ -125,16 +216,16 @@ echo " done."
 # Filtering the list of files to check which are to be updated.
 echo "Downloading individual files... "
 # Retrieve the last update time.
-last=$(date +"%s" -d "$(head -1 "$DATAFILE")")
-for file in $(find "$DESTDIR/.minerva/"); do
+last=$(date +"%s" -d "$(head -1 "$datafile")")
+for file in $(find "$destdir/.minerva/"); do
 
     localfile=${file/.minerva\//}
     localfile=${localfile%.new}
     name=${file#*.minerva/}
     name=${name/.new/}
 
-    # Do not take any files matching in DATAFILE.
-    if grep "$name" "$DATAFILE" > /dev/null 2>&1; then
+    # Do not take any files matching in datafile.
+    if grep "$name" "$datafile" > /dev/null 2>&1; then
         continue
     fi
 
@@ -167,11 +258,11 @@ for file in $(find "$DESTDIR/.minerva/"); do
         mv "$temp1" "$localfile"
     else
         if [ "$answer" == "never" ]; then # Add to files not to download.
-            echo "$localfile" >> "$DATAFILE"
+            echo "$localfile" >> "$datafile"
         fi
     fi
 done
 echo "Done. Your local folder is now synced with Minerva."
 
-mv "$DATAFILE" "$temp1"
-cat "$temp1" | sed "1c $(date)" > "$DATAFILE"
+mv "$datafile" "$temp1"
+cat "$temp1" | sed "1c $(date)" > "$datafile"
